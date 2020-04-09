b35263914bf6bd0107bac11b2472371bfe17c87d
JanWielgus <jan.wielgus12@gmail.com>
2020-04-09 13:20:41 +0200

    Update ITransferable.h

--- ITransferable.h
+++ ITransferable.h
@@ -14,7 +14,7 @@
 class ITransferable
 {
 public:
-	virtual ~ITransferable();
+	virtual ~ITransferable() = 0 {}
 	virtual uint8_t getPacketID() = 0;
 	virtual uint8_t getPacketSize() = 0;
 	virtual FC_GrowingArray<uint8_t*> getBytePointersArray() = 0;
