f31fae5b12f714e06d916357ffbdaab0b84383fb
JanWielgus <jan.wielgus12@gmail.com>
2020-04-09 13:11:03 +0200

    Added ITransferable interface
    Will be used by data packets

---
+++ ITransferable.h
@@ -0,0 +1,24 @@
+// ITransferable.h
+/*
+    Created:	09/04/2020
+    Author:     Jan Wielgus
+*/
+
+#ifndef _ITRANSFERABLE_h
+#define _ITRANSFERABLE_h
+
+#include "arduino.h"
+#include <FC_GrowingArray.h>
+
+
+class ITransferable
+{
+public:
+    virtual uint8_t getPacketID() = 0;
+    virtual uint8_t getPacketSize() = 0;
+    virtual FC_GrowingArray<uint8_t*> getBytePointersArray() = 0;
+};
+
+
+#endif
+
