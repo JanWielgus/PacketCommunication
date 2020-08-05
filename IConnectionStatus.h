56043ab69258899d18ca7c2cc11818fb0662cf6b
Jan Wielgus <jan.wielgus12@gmail.com>
2020-08-05 12:21:02 +0200

    Implementaion of IConnectionStatus interface

---
+++ IConnectionStatus.h
@@ -0,0 +1,27 @@
+/**
+ * @file IConnectionStatus.h
+ * @author Jan Wielgus
+ * @date 2020-07-30
+ * 
+ */
+
+// FIXME: move this file to the communication library
+
+#ifndef ICONNECTIONSTATUS_H
+#define ICONNECTIONSTATUS_H
+
+
+namespace Interfaces
+{
+    class IConnectionStatus
+    {
+    public:
+        virtual ~IConnectionStatus() {}
+
+        virtual uint8_t getConnectionStability() = 0;
+        // TODO: think about other connection statuses (maybe boolean equivalent of method above)
+    };
+}
+
+
+#endif
