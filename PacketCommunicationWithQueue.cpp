e6ad49bdc15df76267802f1eff88bdaad9c73b61
Jan Wielgus <jan.wielgus12@gmail.com>
2020-08-23 16:40:21 +0200

    Implementation of PacketCommunicationWithQueue class in progress
    Basically, only one method left to implement

---
+++ PacketCommunicationWithQueue.cpp
@@ -0,0 +1,68 @@
+/**
+ * @file PacketCommunicationWithQueue.cpp
+ * @author Jan Wielgus
+ * @date 2020-08-22
+ * 
+ */
+
+#include "PacketCommunicationWithQueue.h"
+#include "C:\Users\janwi\Documents\Arduino\libraries\DataStructures\SinkingQueue.h" // FIXME: remove full path
+
+
+PacketCommunicationWithQueue::PacketCommunicationWithQueue(ITransceiver* lowLevelComm, size_t maxQueuedBuffers)
+    : NoQueuePacketCommunication(lowLevelComm)
+{
+    queuedBuffers = new SinkingQueue(maxQueuedBuffers);
+}
+
+
+PacketCommunicationWithQueue::~PacketCommunicationWithQueue()
+{
+    // TODO: release memory for allocated buffers left in queue!
+    delete queuedBuffers;
+}
+
+
+void PacketCommunicationWithQueue::execute()
+{
+    receiveIncomingBuffersToQueue();
+    updateReceiveDataPacketsWithOldestBuffers();
+}
+
+
+
+void PacketCommunicationWithQueue::receiveIncomingBuffersToQueue()
+{
+    while (LowLevelComm->available())
+    {
+        DataBuffer receivedBuffer = LowLevelComm->receiveNextData();
+        if (receivedBuffer.size == 0)
+            continue;
+
+        // Check if packet of this id and size exist on the receive packets pointers list
+        if (getReceiveDataPacketPointer(receivedBuffer.buffer[0], receivedBuffer.size - 1) == nullptr)
+            continue;
+        
+        DataBuffer receivedBufferCopy = createNewBufferAndAllocateMemory(receivedBuffer.size);
+        copyBufferContents(receivedBuffer, receivedBufferCopy);
+        enqueueBuffer(receivedBufferCopy);
+    }
+}
+
+
+void PacketCommunicationWithQueue::updateReceiveDataPacketsWithOldestBuffers()
+{
+    // TODO: implement this method
+
+    // remember to release dynamically allocated memory !!
+}
+
+
+
+void PacketCommunicationWithQueue::enqueueBuffer(DataBuffer buffer)
+{
+    if (queuedBuffers->isFull())
+        delete[] queuedBuffers->dequeue().buffer;
+    
+    queuedBuffers->enqueue(buffer);
+}
---
+++ PacketCommunicationWithQueue.h
@@ -0,0 +1,48 @@
+/**
+ * @file PacketCommunicationWithQueue.h
+ * @author Jan Wielgus
+ * @brief Concrete class that extend NoQueuePacketCommunication class.
+ * This class differs from NoQueuePacketCommunication class only in receiving (execute() method)
+ * Adds queue for receiving data packets, hence there is an option to
+ * send data packets to this library more often or in larger packs once
+ * than this library receives it, data will be gradually received
+ * (oldest data may be lost if queue will overflow).
+ * @date 2020-08-19
+ * 
+ */
+
+// FIXME: figure out better name that show, that this class has queue only for received data!!!
+
+#ifndef PACKETCOMMUNICATIONWITHQUEUE_H
+#define PACKETCOMMUNICATIONWITHQUEUE_H
+
+#include "NoQueuePacketCommunication.h"
+#include "C:\Users\janwi\Documents\Arduino\libraries\DataStructures\IQueue.h"
+
+
+class PacketCommunicationWithQueue : public NoQueuePacketCommunication
+{
+protected:
+    IQueue<DataBuffer>* queuedBuffers; // Buffers on the queue are dynamically allocated and need to be deleted after dequeue
+
+public:
+    PacketCommunicationWithQueue(ITransceiver* lowLevelComm, size_t maxQueuedBuffers);
+    virtual ~PacketCommunicationWithQueue();
+    virtual void execute() override;
+
+protected:
+    void receiveIncomingBuffersToQueue();
+    void updateReceiveDataPacketsWithOldestBuffers();
+
+private:
+    /**
+     * @brief Add buffer to the queuedBuffers queue.
+     * Do not allocate memory. Just store the pointer.
+     * 
+     * @param buffer Buffer to put in the queue.
+     */
+    void enqueueBuffer(DataBuffer buffer);
+};
+
+
+#endif
