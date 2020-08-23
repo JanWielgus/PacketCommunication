545a2036aaf7dc1f7b30d87baedfa0ccdb43e48e
Jan Wielgus <jan.wielgus12@gmail.com>
2020-08-23 15:19:46 +0200

    Added comment to getConnectionStability() method in IConnectionStatus.h

--- IConnectionStatus.h
+++ IConnectionStatus.h
@@ -14,6 +14,11 @@ class IConnectionStatus
 public:
     virtual ~IConnectionStatus() {}
 
+    /**
+     * @brief Checks connection stability.
+     * 
+     * @return conneciton stability in % (0 - no conneciton, 100 - uninterrupted connection)
+     */
     virtual uint8_t getConnectionStability() = 0;
     // TODO: think about other connection statuses (maybe boolean equivalent of method above)
 };
