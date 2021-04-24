8cc1fb3ce2bd7ecad29173e5a891f24858e966a3
Jan Wielgus <jan.wielgus12@gmail.com>
2021-04-24 23:03:44 +0200

    Updated IReceiver

--- IReceiver.h
+++ IReceiver.h
@@ -25,20 +25,20 @@ namespace PacketCommunication
         virtual ~IReceiver() {}
 
         /**
-         * @brief Receive next available data buffer. Call before getReceivedData() method
+         * @brief Receive next available data buffer. Call before getReceived() method
          * to get new data packet each time.
          * @return true if a data packet was received; false if there were no data or
-         * received data was corrupted (getReceivedData() method will return an empty buffer).
+         * received data was corrupted (getReceived() method will return an empty buffer).
          */
-        virtual bool receiveBuffer() = 0;
+        virtual bool receive() = 0;
 
         /**
-         * @brief Call receiveData() method before to get the newest buffer. Can be called
-         * multiple times in a row (to get the same buffer).
+         * @brief Returns the last data received by receive() method (call receive() method
+         * before). Can be called multiple times in a row (to get the same buffer).
          * @return DataBuffer with received data or empty buffer if no data was received.
          * Make your own copy of data in the buffer or just use returned buffer.
          */
-        virtual DataBuffer getReceivedBuffer() = 0;
+        virtual DataBuffer getReceived() = 0;
     };
 }
 
