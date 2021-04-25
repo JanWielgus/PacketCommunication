f3fc15b2143923fb6b6e0b34840ed9e294ee8452
Jan Wielgus <jan.wielgus12@gmail.com>
2021-04-25 19:38:35 +0200

    Refactored StreamComm

--- StreamComm.h
+++ StreamComm.h
@@ -14,67 +14,155 @@
 
 #include "ITransceiver.h"
 #include "DataBuffer.h"
+#include "AutoDataBuffer.h"
+#include "Encoding/COBS.h" // SLIP.h is alternative
+#include "commUtils.h"
+#include <etl/memory.h>
 
 
-class StreamComm : public ITransceiver
+namespace PacketCommunication
 {
-private:
-    static const uint8_t PacketMarker; // definition in cpp
-    const size_t MaxBufferSize;
-    Stream* stream;
+    template <size_t MaxBufferSize>
+    class StreamComm : public ITransceiver
+    {
+        static const uint8_t PacketMarker;
+        Stream* stream;
 
-    // sending helper variables
-    uint8_t* encodeBuffer; // buffer with data after encoding, used by sending methods
+        // sending helper variables:
+        uint8_t encodeBuffer[COBS::getEncodedBufferSize(MaxBufferSize)]; // buffer with data after encoding, used by sending methods
 
-    // receiving helper variables
-    uint8_t* receiveBuffer; // accumulate received bytes into array
-    size_t receiveBufferIndex = 0; // amount of data in receiveBuffer
-    ExtendedDataBuffer decodedData; // received and decoded data
-
-
-public:
-    /**
-     * @brief Construct a new Stream Comm object
-     * 
-     * @param streamPtr Pointer to object that its class implement Arduino Stream interface.
-     * Initialize the stream manually outside this class.
-     * @param bufSize Maximum size of one data packet in bytes (don't forget about place for packet ID).
-     * Default size is 255.
-     */
-    StreamComm(Stream* streamPtr, size_t maxBufferSize = 255);
-    ~StreamComm();
-
-    StreamComm(const StreamComm& other) = delete;
-    StreamComm& operator=(const StreamComm& other) = delete;
-
-    // public interface
-    bool send(const uint8_t* buffer, size_t size) override;
-    bool send(const ExtendedDataBuffer& buffer) override;
-    bool receiveData() override;
-    DataBuffer getReceivedData() override;
+        // receiving helper variables:
+        uint8_t receiveBuffer[MaxBufferSize]; // accumulate received bytes into array
+        size_t receiveBufferIndex = 0; // amount of data in receiveBuffer
+        uint8_t decodedData[MaxBufferSize]; // received and decoded data
+        size_t decodedDataSize = 0;
 
 
-private:
-    /**
-     * @brief Check if passed buffer checksum is correct.
-     * 
-     * @param buffer pointer to the array of data (only data).
-     * @param size size of the array with data (amount of bytes).
-     * @param checksum checksum that that array should have.
-     * @return true if array has the same checksum as checksum in parameter,
-     * false otherwise.
-     */
-    bool checkChecksum(const uint8_t* buffer, size_t size, uint8_t checksum);
+    public:
+        /**
+         * @brief Ctor.
+         * @param streamPtr Pointer to Arduino Stream object.
+         * Initialize the stream manually outside of this class.
+         */
+        explicit StreamComm(Stream* stream);
 
-    /**
-     * @brief Calculate the checksum for passed data buffer.
-     * 
-     * @param buffer pointer to the array with data (only data).
-     * @param size size of the array with data.
-     * @return checksum for the passed data buffer.
-     */
-    uint8_t calculateChecksum(const uint8_t* buffer, size_t size);
-};
+        StreamComm(const StreamComm& other) = delete;
+        StreamComm& operator=(const StreamComm& other) = delete;
+
+        bool send(const uint8_t* buffer, size_t size) override;
+        bool send(const AutoDataBuffer& buffer) override;
+        bool receive() override;
+        DataBuffer getReceived() override;
+    };
+
+
+
+    template <size_t MaxBufferSize>
+    const uint8_t StreamComm<MaxBufferSize>::PacketMarker = 0;
+
+
+    template <size_t MaxBufferSize>
+    StreamComm<MaxBufferSize>::StreamComm(Stream* stream)
+    {
+        this->stream = stream;
+    }
+
+
+    template <size_t MaxBufferSize>
+    bool StreamComm<MaxBufferSize>::send(const uint8_t* buffer, size_t size)
+    {
+        if (buffer == nullptr || size == 0 || size > MaxBufferSize)
+            return false;
+
+        etl::unique_ptr<uint8_t[]> bufferWithChecksum(new uint8_t[MaxBufferSize + 1]);
+        Utils::copyBuffer(bufferWithChecksum, buffer, size);
+        bufferWithChecksum[size] = Utils::calculateChecksum(buffer, size); // add checksum after the last byte
+
+        size_t numEncoded = COBS::encode(bufferWithChecksum, size + 1, encodeBuffer);
+
+        stream->write(encodeBuffer, numEncoded);
+        stream->write(PacketMarker);
+
+        return true;
+    }
+
+
+    template <size_t MaxBufferSize>
+    bool StreamComm<MaxBufferSize>::send(const AutoDataBuffer& buffer)
+    {
+        if (buffer.size == 0 || buffer.size > MaxBufferSize)
+            return false;
+        
+        if (buffer.size == buffer.AllocatedSize)
+            return send(buffer.buffer, buffer.size);
+        
+        if (buffer.size > buffer.AllocatedSize) // in this case data packet is corrupted - memory used is bigger than allocated memory
+            return false;
+        
+        // add checksum after the last byte
+        buffer.buffer[buffer.size] = Utils::calculateChecksum(buffer.buffer, buffer.size);
+
+        size_t numEncoded = COBS::encode(buffer.buffer, buffer.size + 1, encodeBuffer);
+
+        stream->write(encodeBuffer, numEncoded);
+        stream->write(PacketMarker);
+
+        return true;
+    }
+
+
+    template <size_t MaxBufferSize>
+    bool StreamComm<MaxBufferSize>::receive()
+    {
+        while (stream->available() > 0)
+        {
+            uint8_t data = stream->read();
+
+            if (data == PacketMarker)
+            {
+                decodedDataSize = COBS::decode(receiveBuffer, receiveBufferIndex, decodedData);
+
+                receiveBufferIndex = 0; // reset index in the received data buffer
+
+                uint8_t checksum = decodedData[decodedDataSize - 1];
+                bool checksumResult = Utils::checkChecksum(decodedData, decodedDataSize - 1, checksum);
+                if (checksumResult)
+                    decodedDataSize--; // 'remove' checksum from the output buffer
+                else
+                    decodedDataSize = 0; // checksum failed, corrupted buffer
+
+                return decodedDataSize == 0 ? false : true;
+            }
+            else
+            {
+                if (receiveBufferIndex + 1 < MaxBufferSize)
+                    receiveBuffer[receiveBufferIndex++] = data;
+                else
+                {
+                    // ERROR, buffer overflow if we write.
+                    // Received buffer was too big to decode (increase MaxBufferSize).
+
+                    // Finnish receiving this packet and reset the receiveBuffer
+                    while (stream->available() && stream->read() != PacketMarker);
+                    receiveBufferIndex = 0;
+                    decodedDataSize = 0;
+                    return false;
+                }
+            }
+        }
+
+        // No complete buffer was received.
+        decodedDataSize = 0;
+        return false;
+    }
+
+
+    template <size_t MaxBufferSize>
+    DataBuffer StreamComm<MaxBufferSize>::getReceived()
+    {
+        return DataBuffer(decodedData, decodedDataSize);
+    }
+}
 
 
 #endif
