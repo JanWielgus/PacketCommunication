3a168fbf6d5a69b954676910bce4e63ee7bdf3ce
Jan Wielgus <jan.wielgus12@gmail.com>
2021-04-26 12:32:51 +0200

    Refactored Packet class AGAIN

---
+++ Packet.cpp
@@ -0,0 +1,75 @@
+/**
+ * @file Packet.cpp
+ * @author Jan Wielgus (jan.wielgus12@gmail.com)
+ * @date 2021-04-26
+ * 
+ */
+
+#include "Packet.h"
+
+
+using namespace PacketComm;
+
+
+Packet::Packet(uint16_t packetID)
+    : PacketID(packetID)
+{
+}
+
+
+Packet::~Packet()
+{
+}
+
+
+inline uint16_t Packet::getID() const
+{
+    return (uint16_t)PacketID;
+}
+
+
+inline size_t Packet::getSize() const
+{
+    return PacketID.byteSize() + getDataOnlySize();
+}
+
+
+void Packet::setPacketReceivedEvent(IExecutable& packetReceivedEvent)
+{
+    this->packetReceivedEvent = &packetReceivedEvent;
+}
+
+
+inline IExecutable* Packet::getPacketReceivedEvent() const
+{
+    return packetReceivedEvent;
+}
+
+
+size_t Packet::getBuffer(uint8_t* outputBuffer) const
+{
+    uint8_t packetIDSize = PacketID.byteSize();
+    for (uint8_t i = 0; i < packetIDSize; ++i)
+        outputBuffer[i] = PacketID.byteArray()[i];
+    
+    return packetIDSize + getDataOnly(outputBuffer + packetIDSize);
+}
+
+
+bool Packet::updateBuffer(const uint8_t* inputBuffer)
+{
+    if (!checkIfIDMatch(inputBuffer))
+        return false;
+
+    updateDataOnly(inputBuffer + PacketID.byteSize());
+    return true;
+}
+
+
+bool Packet::checkIfIDMatch(const uint8_t* buffer)
+{
+    for (uint8_t i = 0; i < PacketID.byteSize(); i++)
+        if (buffer[i] != PacketID.byteArray()[i])
+            return false;
+    return true;
+}
--- Packet.h
+++ Packet.h
@@ -10,76 +10,96 @@
 #define PACKET_H
 
 #include <IExecutable.h> // TODO: if the rest of the library is not dependent on Tasker, try to remove that dependency
+#include <byteType.h>
 
 
-namespace PacketCommunication
+namespace PacketComm
 {
     class Packet
     {
-    protected:
-        const uint8_t PacketID;
+        const uint16Byte PacketID;
         IExecutable* packetReceivedEvent = nullptr;
 
 
     public:
-        Packet(uint8_t packetID)
-            : PacketID(packetID)
-        {
-        }
-
-        virtual ~Packet() {}
+        explicit Packet(uint16_t packetID);
+        virtual ~Packet();
 
         Packet(const Packet&) = delete;
         Packet& operator=(const Packet&) = delete;
 
 
         /**
-         * @return ID od this data packet.
+         * @return ID of this packet.
          */
-        uint8_t getID() const
-        {
-            return PacketID;
-        }
+        uint16_t getID() const;
 
         /**
          * @return Size of this packet in bytes (total amount of bytes that
          * this packet consists of including packet ID).
          */
-        virtual size_t getSize() const = 0;
-
-        /**
-         * @brief Fill the outputBuffer with packet's internal data.
-         * outputBuffer size have to be at least packet size!
-         * @param outputBuffer Pointer to the array where data will be stored.
-         * @return Size of this packet in bytes so number of values changed
-         * in the outputBuffer (same value that getSize() method would return).
-         */
-        virtual size_t getBuffer(uint8_t* outputBuffer) const = 0;
-
-        /**
-         * @brief Update packet internal buffer with an inputBuffer.
-         * inputBuffer size have to be at least packet size!
-         * @param inputBuffer Pointer to the array of data to update this packet.
-         * @return  
-         */
-        virtual void updateBuffer(const uint8_t* inputBuffer) = 0;
+        size_t getSize() const;
 
         /**
          * @brief packetReceivedEvent will be called just after when this data packet will be received.
          * @param packetReceivedEvent Object that implements IExecutable interface.
          */
-        void setPacketReceivedEvent(IExecutable& packetReceivedEvent)
-        {
-            this->packetReceivedEvent = &packetReceivedEvent;
-        }
+        void setPacketReceivedEvent(IExecutable& packetReceivedEvent);
 
         /**
          * @return Pointer to the previously set event, or nullptr if any event was set. 
          */
-        IExecutable* getPacketReceivedEvent() const
-        {
-            return packetReceivedEvent;
-        }
+        IExecutable* getPacketReceivedEvent() const;
+
+        /**
+         * @brief Fill the outputBuffer with packet's internal data (includes PacketID).
+         * outputBuffer size have to be at least packet size.
+         * @param outputBuffer Pointer to the array where data will be stored.
+         * @return Size of this packet in bytes (same value that
+         * getSize() method would return).
+         */
+        size_t getBuffer(uint8_t* outputBuffer) const;
+
+        /**
+         * @brief Update packet internal buffer with an inputBuffer (inputBuffer have to
+         * contain PacketID, basically have to be exactly what getBuffer() method returned).
+         * inputBuffer size have to be at least packet size.
+         * @param inputBuffer Pointer to the array of data to update this packet.
+         * @return false if inputBuffer dont match this packet ID. True otherwise.
+         */
+        bool updateBuffer(const uint8_t* inputBuffer);
+
+
+    protected:
+        /**
+         * @brief Fill the outputBuffer with packet's internal data only
+         * (data that this packet consists of excluding the PacketID).
+         * @param outputBuffer Pointer to the array where data will be stored.
+         * @return Amount of bytes that this packet consists of (excluding ID).
+         */
+        virtual size_t getDataOnly(uint8_t* outputBuffer) const = 0;
+
+        /**
+         * @return Size of this packet in bytes (total amount of bytes that
+         * this packet consists of EXCLUDING packet ID).
+         */
+        virtual size_t getDataOnlySize() const = 0;
+
+        /**
+         * @brief Update packet internal buffer with an inputBuffer. inputBuffer don't
+         * contain PacketID at all!
+         * @param inputBuffer Pointer to the array of data to update this packet (without PacketID).
+         */
+        virtual void updateDataOnly(const uint8_t* inputBuffer) = 0;
+
+
+    private:
+        /**
+         * @brief Check if beginning of the buffer contains ID of this packet.
+         * @param buffer Buffer which beginning is expected to contain this packet ID.
+         * @return true if buffer contain ID of this packet. False otherwise.
+         */
+        bool checkIfIDMatch(const uint8_t* buffer);
     };
 }
 
