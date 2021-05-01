e472825acf777beab1c8d15684ee99806356fe86
Jan Wielgus <jan.wielgus12@gmail.com>
2021-05-01 14:22:11 +0200

    Implemented EventPacket

---
+++ EventPacket.h
@@ -0,0 +1,52 @@
+/**
+ * @file EventPacket.h
+ * @author Jan Wielgus (jan.wielgus12@gmail.com)
+ * @brief This is a data packet type that don't contain any data
+ * but only a packet received event. Is used to
+ * trigger some action.
+ * @date 2021-01-28
+ * 
+ */
+
+
+#ifndef EVENTPACKET_H
+#define EVENTPACKET_H
+
+#include "Packet.h"
+
+
+namespace PacketComm
+{
+    /**
+     * @brief This is a data packet type that don't contain any data 
+     * but only a packet received event. Is used to
+     * trigger some action.
+     */
+    class EventPacket : public Packet
+    {
+    public:
+        EventPacket(PacketIDType packetID, Callback receivedCallback = nullptr)
+            : Packet(packetID, receivedCallback)
+        {
+        }
+
+
+    protected:
+        size_t getDataOnly(uint8_t*) const override
+        {
+            return 0;
+        }
+
+        size_t getDataOnlySize() const override
+        {
+            return 0;
+        }
+
+        void updateDataOnly(const uint8_t*) override
+        {
+        }
+    };
+}
+
+
+#endif
