2bc16ddd0efced524175118ea578e2f78f534481
Jan Wielgus <jan.wielgus12@gmail.com>
2021-06-11 20:42:34 +0200

    Updated Ping example program

--- PingTest_example/PingTest_example.ino
+++ PingTest_example/PingTest_example.ino
@@ -1,72 +1,80 @@
 /**
  * @file PingTest_example.ino
- * @author Jan Wielgus
- * @brief Test of time that takes to send data packet and get respond.
- * Upload this code on both deivces.
- * @date 2020-11-04
+ * @author your name (you@domain.com)
+ * @brief Test connection between two devices.
+ * Upload this code on both devices.
+ * @date 2021-06-11
  * 
  */
 
-
-#include <PacketCommunicationWithQueue.h>
+#include <BytePacket.h>
 #include <StreamComm.h>
-#include <DataPacket.h>
+#include <PacketCommunication.h>
 #include <SoftwareSerial.h>
 #include <SimpleTasker.h>
-#include <ByteTypes.h>
+#include <byteType.h>
+
+using namespace PacketComm;
+
+const uint16_t MaxBufferSize = 20;
+const float PingInterval_s = 2;
+
+uint32_t pingRequestTime_us;
+uint32_t pingCounter = 0;
 
 
-
-// Sent variables
-uint32Byte startTime;
-
-// Received variables
-uint32Byte respondTime; // not used, because time on both devices is not synchronized
+void pingRequestReceivedCallback();
+void pingReplyReceivedCallback();
 
 
-DataPacket pingRequestPacket(0);
-DataPacket pingReplyPacket(1);
+class PingRequestPacket : public BytePacket
+{
+public:
+    uint32Byte pingRequestCounter = 0;
+
+    PingRequestPacket() : BytePacket(0, pingRequestReceivedCallback)
+    {
+        addByteType(pingRequestCounter);
+    }
+} pingRequestPacket;
+
+
+class PingReplyCounter : public BytePacket
+{
+public:
+    uint32Byte pingReplyCounter;
+
+    PingReplyCounter() : BytePacket(1, pingReplyReceivedCallback)
+    {
+        addByteType(pingReplyCounter);
+    }
+} pingReplyPacket;
+
 
 
 SoftwareSerial softSerial(10, 11); // RX, TX
-StreamComm streamComm(&softSerial, 20);
-NoQueuePacketCommunication comm(&streamComm);
+
+StreamComm<MaxBufferSize> streamComm(&softSerial);
+PacketCommunication comm(&streamComm);
 
 SimpleTasker tasker(5);
-
 
 
 class : public Task
 {
     void execute() override
     {
-        Serial.println("Sending ping request... ");
-        startTime = micros();
-        comm.sendDataPacket(&pingRequestPacket);
+        pingCounter++;
+        Serial.print("Sending ping request ");
+        Serial.print(pingCounter);
+        Serial.println("...");
+
+        pingRequestTime_us = micros();
+
+        pingRequestPacket.pingRequestCounter = pingCounter;
+        comm.send(&pingRequestPacket);
     }
 } pingRequestTask;
-
-
-class : public IExecutable
-{
-    void execute() override
-    {
-        respondTime = micros();
-        comm.sendDataPacket(&pingReplyPacket);
-    }
-} pingRequestReceivedEvent;
-
-
-class : public IExecutable
-{
-    void execute() override
-    {
-        float ping = (micros() - startTime) / 1000.f;
-        Serial.print("\tGot replay. Ping: ");
-        Serial.print(ping);
-        Serial.println("ms");
-    }
-} pingReplyReceivedEvent;
 
 
 
@@ -77,17 +85,10 @@ void setup()
 
     Serial.println("Program has just started!");
 
-    pingRequestPacket.addByteType(startTime);
-    pingRequestPacket.setPacketReceivedEvent(pingRequestReceivedEvent);
+    comm.registerReceivePacket(&pingRequestPacket);
+    comm.registerReceivePacket(&pingReplyPacket);
 
-    pingReplyPacket.addByteType(respondTime);
-    pingReplyPacket.setPacketReceivedEvent(pingReplyReceivedEvent);
-
-    tasker.addTask(&pingRequestTask, 0.5f, 0); // 0.5Hz ping request
-
-    comm.addReceiveDataPacketPointer(&pingRequestPacket);
-    comm.addReceiveDataPacketPointer(&pingReplyPacket);
-
+    tasker.addTask(&pingRequestTask, 1.f / PingInterval_s);
     // communication instance is not added to tasker, because it have to run
     // as fast as possible (it's execute method is directly called in loop())
 }
@@ -95,7 +96,28 @@ void setup()
 
 void loop()
 {
-    tasker.runLoop();
-    comm.execute(); // receive packets as fast as possible
+    tasker.loop();
+    comm.receive();
 }
+
+
+
+
+void pingRequestReceivedCallback()
+{
+    pingReplyPacket.pingReplyCounter = pingRequestPacket.pingRequestCounter;
+    comm.send(&pingReplyPacket);
+}
+
+
+void pingReplyReceivedCallback()
+{
+    float ping = (micros() - pingRequestTime_us) / 1000.f;
+    Serial.print("\tGot replay ");
+    Serial.print(pingReplyPacket.pingReplyCounter);
+    Serial.print(". Ping: ");
+    Serial.print(ping);
+    Serial.println("ms");
+}
+
 
