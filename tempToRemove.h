2e84f6f3f383aafc915f4c9eac2583ed8aec4159
Jan Wielgus <jan.wielgus12@gmail.com>
2021-07-26 22:36:02 +0200

    tempToRemove file update

--- tempToRemove.h
+++ tempToRemove.h
@@ -1,10 +1,22 @@
+#pragma once
 
 #define ENABLE_TEMP
 
 #ifdef ENABLE_TEMP
 
+/*
 #include <cstdint>
 #include <cstdlib>
+*/
+
+typedef long int32_t;
+typedef int int16_t;
+typedef short int8_t;
+typedef unsigned long uint32_t;
+typedef unsigned int uint16_t;
+typedef unsigned short uint8_t;
+typedef uint32_t size_t;
+
 
 
 class Stream // TODO: temp
@@ -15,5 +27,8 @@ public:
     bool write(uint8_t*, size_t);
     bool write(uint8_t);
 };
+
+template <class T>
+T constrain(T, T, T);
 
 #endif
