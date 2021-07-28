96af4a05e9dee80748f65f0fc88055d1a78b90d9
Jan Wielgus <jan.wielgus12@gmail.com>
2021-07-28 14:58:34 +0200

    New esp8266wifi comm class refactoring

--- ESP8266WiFiComm/ESP8266WiFiComm.h
+++ ESP8266WiFiComm/ESP8266WiFiComm.h
@@ -10,80 +10,90 @@
 #define ESP8266WIFICOMM_H
 
 #include "ITransceiver.h"
+#include "DataBuffer.h"
+#include "AutoDataBuffer.h"
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 
 
-class ESP8266WiFiComm : public ITransceiver
+namespace PacketComm
 {
-private:
-    WiFiUDP udp;
-    IPAddress localIPAddress;
-    IPAddress targetIPAddress;
+    class ESP8266WiFiComm : public ITransceiver
+    {
+    private:
+        WiFiUDP udp;
+        IPAddress localIPAddress;
+        IPAddress targetIPAddress;
 
-    bool sendAlwaysToLastSender_flag = true; // by default
-    bool updBeginned_flag = false;
+        bool sendAlwaysToLastSender_flag = true; // by default
+        bool updBeginned_flag = false;
 
-    // config
-	const uint16_t Port;
+        // config
+        const uint16_t Port;
 
-    ExtendedDataBuffer receiveBuffer;
-
-
-public:
-    ESP8266WiFiComm(uint16_t port, size_t maxPacketSize);
-
-    // interface
-    bool send(const uint8_t* buffer, size_t size) override;
-    bool receiveData() override;
-    DataBuffer getReceivedData() override;
-
-    /**
-     * @brief Set the IP address that all next packets will be send to.
-     * @param ipAddress IP address.
-     */
-    void setTargetIPAddress(IPAddress ipAddress);
-
-    /**
-     * @brief Set the IP address that all next packets will be send to.
-     * @param ipAddress IP address in octets divided form.
-     */
-    void setTargetIPAddress(uint8_t first_octet, uint8_t second_octet, uint8_t third_octet, uint8_t fourth_octet);
-
-    /**
-     * @brief Packets always will be sent to the latest sender
-     * (you have to receive at least one packet to send packets correctly).
-     */
-    void setTargetIPAddressAlwaysToSender();
-
-    /**
-     * @brief Check if WiFi connection was established. 
-     */
-    bool isConnected() const;
-
-    /**
-     * @return IP address of this device.
-     */
-    IPAddress getLocalIP() const;
+        AutoDataBuffer receiveBuffer;
 
 
-private:
-    /**
-     * @brief Should be called if wifi connection was established.
-     * Starts listening on the local port.
-     * @return false if WiFi connection was not established
-     * or UDP was already successfully beginned,
-     * otherwise returns true.
-     */
-    bool beginUDP();
+    public:
+        ESP8266WiFiComm(uint16_t port, size_t maxPacketSize);
 
-    /**
-     * @brief Return true if after connection being established
-     * udp was beginned and started listening on the Port.
-     */
-    bool checkIfBeginnedUDP() const;
-};
+        bool send(const uint8_t* buffer, size_t size) override;
+        bool receive() override;
+        DataBuffer getReceived() override;
+
+        /**
+         * @brief Set the IP address that all next packets will be send to.
+         * @param ipAddress IP address.
+         */
+        void setTargetIPAddress(IPAddress ipAddress);
+
+        /**
+         * @brief Set the IP address that all next packets will be send to.
+         * @param ipAddress IP address in octets divided form.
+         */
+        void setTargetIPAddress(uint8_t first_octet, uint8_t second_octet, uint8_t third_octet, uint8_t fourth_octet);
+
+        /**
+         * @brief Packets always will be sent to the latest sender
+         * (you have to receive at least one packet to send packets correctly).
+         */
+        void setTargetIPAddressAlwaysToSender();
+
+        /**
+         * @brief Check if WiFi connection was established. 
+         */
+        bool isConnected() const;
+
+        /**
+         * @return IP address of this device.
+         */
+        IPAddress getLocalIP() const;
+
+
+    private:
+        /**
+         * @brief Should be called if wifi connection was established.
+         * Starts listening on the local port.
+         * @return false if WiFi connection was not established
+         * or UDP was already successfully beginned,
+         * otherwise returns true.
+         */
+        bool beginUDP();
+
+        /**
+         * @brief Return true if after connection being established
+         * udp was beginned and started listening on the Port.
+         */
+        bool beginnedUDP() const;
+    };
+
+
+
+    inline bool ESP8266WiFiComm::beginnedUDP() const
+    {
+        return updBeginned_flag;
+    }
+}
 
 
 #endif
-
