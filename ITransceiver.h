d8748f24a4b524aab90acb291a5dac3b3a9ada47
Jan Wielgus <jan.wielgus12@gmail.com>
2022-06-09 18:59:13 +0200

    Moved ITransmitter and IReceiver into ITransceiver.h

--- ITransceiver.h
+++ ITransceiver.h
@@ -10,12 +10,82 @@
 #ifndef ITRANSCEIVER_H
 #define ITRANSCEIVER_H
 
-#include "ITransmitter.h"
-#include "IReceiver.h"
+#include "DataBuffer.h"
 
 
 namespace PacketComm
 {
+    /**
+     * @brief Interface for classes that enables sending
+     * uint8_t arrays (buffers).
+     */
+    class ITransmitter
+    {
+    public:
+        virtual ~ITransmitter() {}
+
+        /**
+         * @brief Send data from the buffer with provided size.
+         * @param buffer Pointer to the data buffer.
+         * @param size Amount of data to send (size of the buffer).
+         * @return true if data were sent, false otherewise.
+         */
+        virtual bool send(const uint8_t* buffer, size_t size) = 0;
+
+        /**
+         * @brief Send data from provided data buffer.
+         * @param buffer Buffer to send.
+         * @return true if data were sent, false otherewise.
+         */
+        bool send(const DataBuffer& buffer)
+        {
+            return send(buffer.buffer, buffer.size);
+        }
+
+        /**
+         * @brief Send data from provided data buffer.
+         * This method could work faster than other send(..) methods
+         * if AllocatedSize will be bigger than size of the data in buffer
+         * (some low-level classes have to copy data to bigger buffers
+         * in order to add some information).
+         * @param buffer Buffer to send.
+         * @return true if data were sent, false otherewise.
+         */
+        virtual bool send(const AutoDataBuffer& buffer)
+        {
+            return send(buffer.buffer, buffer.size);
+        }
+    };
+
+
+
+    /**
+     * @brief Interface for classes that enables receiving
+     * uint8_t arrays (buffers).
+     */
+    class IReceiver
+    {
+    public:
+        virtual ~IReceiver() {}
+
+        /**
+         * @brief Receive next available data buffer. Call before getReceived() method
+         * to get new data packet each time.
+         * @return true if a data packet was received;
+         * false otherwise (getReceived() method will return an empty buffer).
+         */
+        virtual bool receive() = 0;
+
+        /**
+         * @brief Returns the last data received by receive() method (call receive() method
+         * before). Can be called multiple times in a row (to get the same buffer).
+         * @return DataBuffer with received data or empty buffer if no data was received.
+         */
+        virtual const DataBuffer getReceived() = 0;
+    };
+
+
+
     /**
      * @brief Iterface for classes that enables
      * sending and receiving array of uint8_t values.
